228411fc9a2d67f66148c5897f7f2c0e89e750e3
ayazomar <ayazneymarjr@gmail.com>
2023-04-06 10:46:49 +0300

    harjoitus2 tehty

---
+++ teht2/teht2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1= new Opiskelija("Ayaz", "DATV22", 0);
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}
