37ec922db5ea7e17e5a963b8e8b85b0e71a27d53
ayazomar <ayazneymarjr@gmail.com>
2023-04-06 13:47:18 +0300

    teht4tehty

---
+++ albumi/albumi/Program.cs
@@ -0,0 +1,24 @@
+﻿using albumi;
+using albumi;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Album albumit = new Album();
+
+        albumit.artisti = "Nle Choppa";
+        albumit.nimi = "Top shotta";
+        albumit.genre = "Hip Hop";
+        albumit.hinta = 10;
+        albumit.Kappaleenlista("Capo");
+        albumit.Kappaleenlista("Beat box");
+        albumit.Kappaleenlista("Shotta Flow");
+        albumit.Kappaleenlista("Famous hoes");
+        albumit.Kappaleenlista("Do it Again");
+        albumit.Kappaleenlista("Little Miss");
+
+        albumit.TulostaDataa();
+        albumit.MusiikinTulostus();
+    }
+}
---
+++ albumi/albumi/album.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace albumi
+{
+    internal class Album
+    {
+        public string artisti;
+
+        public string nimi;
+
+        public string genre;
+
+        public int hinta;
+
+        public List<string> kappale=new List<string>();
+        public void Kappaleenlista(string kappaleet)
+        {
+            kappale.Add(kappaleet);
+        }
+        public void TulostaDataa()
+        {
+            Console.WriteLine("albumi: ");
+            Console.WriteLine("artisti: " + artisti);
+            Console.WriteLine("nimi: " + nimi);
+            Console.WriteLine("genre: " + genre);
+            Console.WriteLine("hinta: " + hinta + " euroa");
+            Console.WriteLine();
+        }
+
+        public void MusiikinTulostus()
+        {
+            Console.WriteLine("\n*" + kappale[0]);
+            Console.WriteLine("*"+kappale[1]);
+            Console.WriteLine("*"+kappale[2]);
+            Console.WriteLine("*"+kappale[3]);
+            Console.WriteLine("*"+kappale[4]);
+            Console.WriteLine("*" +kappale[5]);
+        }
+
+    }
+}
