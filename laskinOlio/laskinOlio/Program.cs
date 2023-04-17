4f453c34b0706efb5df717c2242002123f7c5429
ayazomar <ayazneymarjr@gmail.com>
2023-04-17 10:02:42 +0300

    teht5 tehty

---
+++ laskinOlio/laskinOlio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tehtävä5;
+
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            float a = 3.5f;
+
+            float b = 2.5f;
+
+            float summa = laskin.Summa(a, b);
+            float erotus = laskin.Erotus(a, b);
+            float kertolasku = laskin.Kertolasku(a, b);
+            float jakolasku = laskin.Jakolasku(a, b);
+            Console.WriteLine("Numeroiden " +a+" ja "+b);
+
+            Console.WriteLine("Summa: " +summa);
+
+            Console.WriteLine("Erotus: " +erotus);
+
+            Console.WriteLine("Kertolasku: " + kertolasku);
+
+
+            Console.WriteLine("Jakolask: " + jakolasku);
+        }
+    }
